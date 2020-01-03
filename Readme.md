0a1cd4ef63e364b156b6b4955465c6e5fc9f5780
Yufei Kang <kyf0722@gmail.com>
2020-01-03 14:47:29 +0900

    init

---
+++ Readme.md
@@ -0,0 +1,23 @@
+# 海外使用网易云音乐的一种方式
+使用nginx反向代理的方式访问muisc.163.com,用户本地需要将此域名的dns解析重定向到nginx所在服务器。因为网易云音乐已经完全使用https，所以需要使用伪造的https证书。
+
+* 生成证书
+`pip3 install mitmproxy`
+`python3 ./gen_cert.py`
+
+   证书生成后在cert目录
+
+* 信任证书
+myca-ca开头的为ca证书，需要设置操作系统信任此CA（具体方法请Google一下），不同后缀是不同格式。各个平台使用的格式不同。
+**因为CA信任会涉及到网络安全问题（中间人攻击），所以请使用自己生成的CA，请勿使用他人的CA**
+
+* 部署NGINX
+将 muisc开头的2个文件复制到/etc/ngixn目录下；
+将 netease.conf 复制到/etc/nginx/sites-enabled目录下；
+重启nginx。
+
+* 本地DNS重定向
+修改本地hosts文件，加入
+```<NGINX SERVER IP> muisc.163.com```
+
+OK 了
---
+++ gen_cert.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+
+from mitmproxy.certs import CertStore
+import OpenSSL
+import os
+
+CA_DIR = './cert'
+
+if not os.path.exists(CA_DIR):
+    os.makedirs(CA_DIR)
+
+certstore = CertStore.from_store(CA_DIR, 'myca')
+
+cert, pkey, ca_path = certstore.get_cert(b'music.163.com', [b'music.163.com'])
+
+with open(os.path.join(CA_DIR, 'muisc.163.com.cert.pem'), 'wb') as f:
+    f.write(cert.to_pem())
+
+with open(os.path.join(CA_DIR, 'muisc.163.com.key.pem'), 'wb') as f:
+    f.write(OpenSSL.crypto.dump_privatekey(OpenSSL.crypto.FILETYPE_PEM, pkey))
+
+print("Success!!")
